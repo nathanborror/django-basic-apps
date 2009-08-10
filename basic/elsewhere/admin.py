e6549d719c16a5491dff99c04b5e166557f0b129
kevin fricovsky <kevin@howiworkdaily.com>
2009-08-09 21:08:16 -0400

    added list_editable feature to admin to allow editing sorting preferences in place

--- basic/elsewhere/admin.py
+++ basic/elsewhere/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from basic.elsewhere.models import *
 
+
+class SocialNetworkProfileAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'url', 'sort_order', )
+    list_editable = ('sort_order',)
+
 admin.site.register(Category)
-admin.site.register(SocialNetworkProfile)
+admin.site.register(SocialNetworkProfile, SocialNetworkProfileAdmin)
