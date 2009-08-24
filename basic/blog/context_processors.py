9ae67d20d84bc8d105d66b7b978b792724e493a2
kevin fricovsky <kevin@montylounge.com>
2009-08-24 00:08:36 -0400

    initial commit of blog_settings context_processors

---
+++ basic/blog/context_processors.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from basic.blog.models import Category
+from django.core.cache import cache
+from basic.blog.models import Settings
+from basic.elsewhere.models import SocialNetworkProfile
+
+def blog_settings(request):
+    """
+    Adds settings information to the context.
+    
+    To employ, add the basic_settings method reference to your project 
+    settings TEMPLATE_CONTEXT_PROCESSORS.
+    
+    Example:
+        TEMPLATE_CONTEXT_PROCESSORS = (
+            ...
+            "basic.blog.context_processors.blog_settings",
+        )
+    """
+    
+    site_id = settings.SITE_ID    
+    key = 'basic.blog.settings:%s' % site_id
+    blog_settings = cache.get(key, None)
+    if blog_settings is None:
+        blog_settings = Settings.get_current()
+        cache.set(key, blog_settings)
+        
+    return {
+        'BLOG_SETTINGS': blog_settings,
+    }
