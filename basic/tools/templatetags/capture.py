eab40da70d2fe51af49464021f8f8779549d0081
Nathan Borror <nathan@playgroundblues.com>
2009-12-18 23:16:37 -0600

    Adding some docs to the capture tag.

--- basic/tools/templatetags/capture.py
+++ basic/tools/templatetags/capture.py
@@ -3,16 +3,6 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-@register.tag
-def capture(parser, token):
-    """{% capture as [foo] %}"""
-    bits = token.split_contents()
-    if len(bits) != 3:
-        raise template.TemplateSyntaxError("'capture' node requires `as (variable name)`.")
-    nodelist = parser.parse(('endcapture',))
-    parser.delete_first_token()
-    return CaptureNode(nodelist, bits[2])
-
 class CaptureNode(template.Node):
     def __init__(self, nodelist, varname):
         self.nodelist = nodelist
@@ -22,3 +12,26 @@ class CaptureNode(template.Node):
         output = self.nodelist.render(context)
         context[self.varname] = mark_safe(output.strip())
         return ''
+
+
+@register.tag('capture')
+def do_capture(parser, token):
+    """
+    Captures content into a context variable.
+    
+    Syntax:
+
+        {% capture as [foo] %}{% endcapture %}
+
+    Usage:
+        
+        {% capture as content %}
+            {% block content %}{% endblock %}
+        {% endcapture %}
+    """
+    bits = token.split_contents()
+    if len(bits) != 3:
+        raise template.TemplateSyntaxError("'capture' node requires `as (variable name)`.")
+    nodelist = parser.parse(('endcapture',))
+    parser.delete_first_token()
+    return CaptureNode(nodelist, bits[2])
