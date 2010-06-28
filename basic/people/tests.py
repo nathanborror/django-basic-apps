c2665e6f585c299647e0777274243c5b4cf71499
Julian Moritz <jumo@gmx.de>
2010-06-28 20:33:42 +0800

    changed the people/tests.py to resolve()'d urls, made tests run.

--- basic/people/tests.py
+++ basic/people/tests.py
@@ -1,15 +1,16 @@
 """
 >>> from django.test import Client
 >>> from basic.people.models import Person, Quote, PersonType
+>>> from django.core.urlresolvers import reverse
 
 >>> c = Client()
 >>> p = Person.objects.create(first_name='Nathan', last_name='Borror', slug='nathan-borror')
 
->>> r = c.get('/people/')
+>>> r = c.get(reverse('person_list'))
 >>> r.status_code
 200
 
->>> r = c.get('/people/nathan-borror/')
+>>> r = c.get(reverse('person_detail', kwargs={'slug': 'nathan-borror'}))
 >>> r.status_code
 200
 """
