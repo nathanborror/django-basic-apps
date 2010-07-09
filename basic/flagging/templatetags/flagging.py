d1ddc79be504e23718ae6f6738834f2ab921b5ea
Nathan Borror <nathan@nathanborror.com>
2010-07-09 18:44:22 -0500

    Adding a template tag to flagging that lets you return a users flag for a particular object given a flag type.

--- basic/flagging/templatetags/flagging.py
+++ basic/flagging/templatetags/flagging.py
@@ -1,7 +1,11 @@
 from django import template
+from django.db import models
 from django.core.urlresolvers import reverse
 from django.contrib.contenttypes.models import ContentType
 
+from basic.tools.templatetags.utils import parse_ttag
+
+Flag = models.get_model('flagging', 'flag')
 register = template.Library()
 
 
@@ -43,3 +47,39 @@ def unflag_url(obj, slug):
         'model': content_type.model,
         'object_id': obj.pk
     })
+
+
+class GetFlags(template.Node):
+    def __init__(self, object_name, user, slug, varname):
+        self.object_name = object_name
+        self.user = user
+        self.slug = slug
+        self.varname = varname
+
+    def render(self, context):
+        obj = template.resolve_variable(self.object_name, context)
+        user = template.resolve_variable(self.user, context)
+        slug = template.resolve_variable(self.slug, context)
+        content_type = ContentType.objects.get_for_model(obj)
+        try:
+            flag = Flag.objects.get(flag_type__slug=slug, content_type=content_type, object_id=obj.id, user=user)
+        except Flag.DoesNotExist:
+            flag = None
+        context[self.varname] = flag
+        return ''
+
+@register.tag('get_flags')
+def do_get_flags(parser, token):
+    """
+    Get flags for an object.
+
+    Syntax:
+        {% get_flags for [object] by [user] type [type-slug] as [varname] %}
+
+    Example:
+        {% get_flags for object by user type "favorites" as flag %}
+    """
+    tags = parse_ttag(token, ['for', 'by', 'type', 'as'])
+    if len(tags) != 5:
+        raise template.TemplateSyntaxError, '%r tag has invalid arguments' % tags['tag_name']
+    return GetFlags(object_name=tags['for'], user=tags['by'], slug=tags['type'], varname=tags['as'])
